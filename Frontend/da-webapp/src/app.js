401cdbcb27dde21128832ba0bb74a8f97df1897d
matthiasp <matthias01123@gmail.com>
2016-02-02 17:18:41 +0000

    Make token height no more than X, scrollbar appears if too long.

--- Frontend/da-webapp/src/app.js
+++ Frontend/da-webapp/src/app.js
@@ -18,6 +18,9 @@
 // let token text scroll when it gets too big, just in case.
 // add maxlength on javascript side using database limits and display an error.
 
+// add functionality to set instructor by id
+// use jshint on all javascript
+
 // ***************************************************************************************** //
 
 (function () {
