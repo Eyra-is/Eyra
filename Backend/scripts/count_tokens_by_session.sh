9313e33ab3456e6e90be499c237bd5c5e5d81e56
mattixpet <matthias01123@gmail.com>
2016-02-05 10:44:17 +0000

    Simple convenience scripts.

---
+++ Backend/scripts/count_tokens_by_session.sh
@@ -0,0 +1,5 @@
+cd ../server-interface/recordings
+for ses in $(ls -1)
+do
+    echo "$ses: "$(ls -1 $ses | (expr $(wc -l) / 2))
+done
---
+++ Backend/scripts/count_total_tokens.sh
@@ -0,0 +1,2 @@
+cd ../server-interface/recordings
+ls -1 session_* | (expr $(wc -l) / 2 - $(ls . | wc -l))
