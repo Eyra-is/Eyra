9cd2c3f6618a7836c9ddb5f5f3812813ff668dbf
mattixpet <matthias01123@gmail.com>
2016-02-10 16:00:31 +0000

    fix info controller

--- Frontend/da-webapp/src/controllers/info.controller.js
+++ Frontend/da-webapp/src/controllers/info.controller.js
@@ -4,7 +4,7 @@
 angular.module('daApp')
 .controller('InfoController', InfoController);
 
-InfoController.$inject = [$scope, $rootScope];
+InfoController.$inject = ['$scope', '$rootScope'];
 
 function InfoController($scope, $rootScope) {
   var infoCtrl = this;
