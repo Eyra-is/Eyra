7bf60fb01dfcb804f6f45bcaeeff264c7a524341
mattixpet <matthias01123@gmail.com>
2016-03-02 17:24:11 +0000

    Script to process the sorted sentences from MIM prompts. and turn them into tokens.

---
+++ Backend/scripts/processMIMTokens.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import re
+
+def process(sortedSentDir, lowerWCBound, upperWCBound, dest):
+    with open(dest, 'w', encoding='utf8') as f:
+        for i in range(int(lowerWCBound), int(upperWCBound)+1):
+            with open(os.path.join(sortedSentDir, str(i), 'sentences.words'), 'r', encoding='utf8') as tmp:
+                f.write(extractSentences(tmp.read().splitlines()))
+
+def extractSentences(data):
+    """
+    Expects data on format as a list of all lines in sentences.words in the MIM prompts without \n. 
+    Extracts them by returning token1\ntoken2\ntoken3 etc.. with one sentence each line.
+    """
+    out = ''
+    for line in data:
+        words = line.split(' ')
+        words = filter(filterOutPuncuation, words)
+        words = list(words)
+        if len(words) >= 1:
+            out += ' '.join(words) + '\n'
+    return out
+
+
+def filterOutPuncuation(x):
+    if re.match('^[\w\s]+$', x, re.UNICODE) is not None\
+       and '_' not in x:
+        return True
+    return False
+
+def run():
+    if len(sys.argv) < 5:
+        print( 
+'Usage: python %s sortedSentDir lowerWCBound upperWCBound dest\n\
+Description: Processes sorted MIM tokens, using the sorted/X/sentences.words files.\n' % sys.argv[0]
++
+'sortedSentDir is the directory containing the folders 1,2,3,4,5,... e.g. /yourpath/MIM_prompts/sent_free/sentences/sorted\n\
+lower and upper WCBound are the number of words per sentence you want. E.g. 5 and 10 would give you all sentences with\
+between 5 and 10 words inclusive.\n\
+dest is the destination file with the output tokens, one on each line.' 
+             )
+        return
+    else:
+        process(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
+if __name__ == '__main__':
+    run()
