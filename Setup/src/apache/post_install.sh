1d4e5676f99fe04a8122d2e5056b7a64dbb00de8
mattixpet <matthias01123@gmail.com>
2016-03-08 12:50:16 +0000

    Fix for mpm_worker setup in apache server.

--- Setup/src/apache/post_install.sh
+++ Setup/src/apache/post_install.sh
@@ -14,7 +14,7 @@ for i in $(ls -1 /etc/apache2/sites-available/); do
   sudo a2dissite ${i} 
 done && \
 sudo a2ensite datatool.conf && \
-sudo a2dismod mpm_prefork && \
+sudo a2dismod mpm_prefork mpm_event && \
 sudo a2enmod ssl wsgi mpm_worker && \
 sudo service apache2 restart && suc || err
 
