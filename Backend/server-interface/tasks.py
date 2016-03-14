7209acd0bc197c0a1c7b27cf3d9b4944cfd22933
Robert Kjaran <robert@kjaran.com>
2016-03-14 13:01:52 +0000

    Backend: added some utils for task management

---
+++ Backend/server-interface/tasks.py
@@ -0,0 +1,20 @@
+from celery import Celery
+from app import app
+
+# TODO: Move to a config file
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
