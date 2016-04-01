82fa0f304fe7ff6bec1669c0c51f9feb319da671
sveinne <sveinne67@gmail.com>
2016-04-01 16:25:56 +0000

    constants changed again

--- Frontend/da-webapp/src/services/utility.service.js
+++ Frontend/da-webapp/src/services/utility.service.js
@@ -19,8 +19,8 @@ function utilityService(logger) {
   var CONSTANTS = { 
     // sentinel value for invalid recordings
     'invalidTitle' : 'no_data.wav',
-    'tokenThreshold' : 40,
-    'tokenGetCount' : 150,
+    'tokenThreshold' : 400,
+    'tokenGetCount' : 1500,
     'QCAccThreshold' : 0.2,
     'QCFrequency' : 5, // per sessions sent
     'QCInitRecThreshold' : 10, // recording count before QC can report, adjustment period for speaker
