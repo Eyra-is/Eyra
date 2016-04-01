6baad0edcc8e005f1c5d9db3b0c94eb6f52e9868
sveinne <sveinne67@gmail.com>
2016-04-01 12:17:36 +0000

    Correcting token loading message

--- Frontend/da-webapp/src/services/utility.service.js
+++ Frontend/da-webapp/src/services/utility.service.js
@@ -19,11 +19,11 @@ function utilityService(logger) {
   var CONSTANTS = { 
     // sentinel value for invalid recordings
     'invalidTitle' : 'no_data.wav',
-    'tokenThreshold' : 400,
-    'tokenGetCount' : 1500,
+    'tokenThreshold' : 40,
+    'tokenGetCount' : 150,
     'QCAccThreshold' : 0.2,
     'QCFrequency' : 5, // per sessions sent
-    'QCInitRecThreshold' : 10, // recording count before QC can report, adjustment period for speaker
+    'QCInitRecThreshold' : 5, // recording count before QC can report, adjustment period for speaker
     'TokenAnnouncementFreq' : 50,
     'TokenCountGoal' : 260
   };
