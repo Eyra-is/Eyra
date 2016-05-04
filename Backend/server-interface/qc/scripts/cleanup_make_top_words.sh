7f671f5ca5319786053d04c29546a331f5a8b4ec
mattixpet <matthias01123@gmail.com>
2016-05-04 16:34:50 +0000

    Add data for javanese cleanup.

---
+++ Backend/server-interface/qc/scripts/cleanup_make_top_words.sh
@@ -0,0 +1,1 @@
+../../../scripts/data_prep/utils/sym2int.pl --map-oov "<UNK>" ../modules/CleanupModule/data/words.syms <(cut -d' ' -f 2- jv_toks | tr '[:upper:]' '[:lower:]') | awk 'BEGIN{total=0;}{a[$1]++; total++;}END{for(k in a)printf "%.5f %d\n", a[k]/total,k}' RS=" |\n" | sort -n -r | head -101 | tail -n 100 > ../modules/CleanupModule/data/top_words.int
