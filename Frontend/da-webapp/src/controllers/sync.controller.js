2c0c6f554be96955d34a9948ac6f750345143b46
mattixpet <matthias01123@gmail.com>
2016-05-08 00:03:04 +0700

    Now displays recs not yet synced, but stored in local database on sync screen.

--- Frontend/da-webapp/src/controllers/sync.controller.js
+++ Frontend/da-webapp/src/controllers/sync.controller.js
@@ -26,29 +26,62 @@ function SyncController($rootScope, $scope, dataService, deliveryService, localD
   $scope.hide_sync = false;
   $scope.hide_wifi_msg = true;
 
-  $scope.recsDelivered = dataService.get('recsDelivered') || 0;
+  $scope.tokensRead = 0;
+  $scope.recsDelivered = 0;
+  $scope.recsSaved = 0;
 
   activate();
   
   //////////
 
   function activate() {
+    // TODO move this token logic to a service, as well as logic in rec controller
+
     // get tokensRead
     var speakerInfo = dataService.get('speakerInfo');
-    var tokensRead;
     if (speakerInfo) {
-      tokensRead = speakerInfo.tokensRead;
+      var tokensRead = speakerInfo.tokensRead;
       if (!tokensRead) {
         // get from ldb
         dbService.getSpeaker(dataService.get('speakerName')).then(function(speaker){
           if (speaker) {
             tokensRead = speaker.tokensRead;
+            $scope.tokensRead = tokensRead || $scope.tokensRead;
           }
         },
         util.stdErrCallback);
+      } else {
+        $scope.tokensRead = tokensRead;
       }
     }
-    $scope.tokensRead = tokensRead || 0;
+
+    // get recsDelivered
+    var recsDelivered = dataService.get('recsDelivered');
+    if (!recsDelivered) {
+      dbService.getSpeaker(dataService.get('speakerName')).then(function(speaker){
+        if (speaker) {
+          $scope.recsDelivered = speaker.recsDelivered || $scope.recsDelivered;
+        }
+      },
+      util.stdErrCallback);
+    } else {
+      $scope.recsDelivered = recsDelivered;
+    }
+
+    // get tokens saved locally, changes $scope.recsSaved
+    updateRecsSaved();
+  }
+
+  function updateRecsSaved() {
+    dbService.getRecsSaved().then(
+      function success(count) {
+        $scope.recsSaved = count || 0;
+      },
+      function error(error) {
+        $scope.recsSaved = 0;
+        logger.error(error);
+      }
+    );    
   }
 
   // sends all available sessions from local db to server, one session at a time
@@ -66,6 +99,8 @@ function SyncController($rootScope, $scope, dataService, deliveryService, localD
     $scope.msg = result ? 'Sync complete.' : 'Sync failed.';
     $scope.hide_sync = false;
     $scope.recsDelivered = dataService.get('recsDelivered') || $scope.recsDelivered || 0;
+    updateRecsSaved();
+    
     if (!result){
       $scope.wifi_msg = 'Please connect device to wifi or ethernet.';
       $scope.hide_recording = true;
