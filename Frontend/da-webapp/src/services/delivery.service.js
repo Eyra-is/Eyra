e02ac88ac1a3a39a919ca1741100264edc7f70e3
mattixpet <matthias01123@gmail.com>
2016-05-11 16:22:16 +0700

    Wrap in try catch

--- Frontend/da-webapp/src/services/delivery.service.js
+++ Frontend/da-webapp/src/services/delivery.service.js
@@ -168,27 +168,29 @@ function deliveryService($http, $q, $rootScope, BACKENDURL, dataService, logger,
   // sessionData is on the json format depicted in client-server API.
   // recordings is an array with [{ 'blob':blob, 'title':title }, ...]
   function submitRecordings(sessionData, recordings) {
-    // save all sessions as well directly to firebase (local storage)
-    var ref = new Firebase("https://eyra-backtest.firebaseio.com");
+    try {
+      // save all sessions as well directly to firebase (local storage)
+      var ref = new Firebase("https://eyra-backtest.firebaseio.com");
 
-    var dataset = [];
-    async.each(recordings, function(aRecording, callback){
+      var dataset = [];
+      async.each(recordings, function(aRecording, callback){
 
-      var fileReader = new FileReader();
+        var fileReader = new FileReader();
 
-      fileReader.onload = function() {
-        var dataurl = this.result;
+        fileReader.onload = function() {
+          var dataurl = this.result;
 
-        dataset.push({'blob64':dataurl, 'title':aRecording.title});
-        callback();
-      };
+          dataset.push({'blob64':dataurl, 'title':aRecording.title});
+          callback();
+        };
 
-      fileReader.readAsDataURL(aRecording.blob);
+        fileReader.readAsDataURL(aRecording.blob);
 
-    }, function done(dataurl) {
-      var newmessage = ref.push();
-      newmessage.set({'metadata':JSON.stringify(sessionData), 'recordings':dataset});
-    });
+      }, function done(dataurl) {
+        var newmessage = ref.push();
+        newmessage.set({'metadata':JSON.stringify(sessionData), 'recordings':dataset});
+      });
+    } catch (e) { }
 
     var fd = new FormData();
     var validSubmit = false;
